5bc86752f9b100d0ff375fee141b6035d060441b
uswa.r <uswa.r@outlook.com>
2022-09-05 19:09:27 +0500

    event listener added

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
 const info = document.getElementById('info');
+
+const preBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function() {
+    console.log("hello");
+})
