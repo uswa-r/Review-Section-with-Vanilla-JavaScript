a174b11beb5f3547bc523743b598c46f5a6e04a1
uswa.r <uswa.r@outlook.com>
2022-09-05 18:51:56 +0500

    first commit

---
+++ database.js
@@ -0,0 +1,22 @@
+ const reviews = [{
+         id: 1,
+         name: "John Doe",
+         job: "Engineer",
+         img: "1.jpeg",
+         text: "loream Ipsum jajdasjdhasjdhajdhajhd."
+     },
+     {
+         id: 2,
+         name: "Jonn Doe",
+         job: "Engineer",
+         img: "1.jpeg",
+         text: "loream Ipsum jajdasjdhasjdhajdhajhd."
+     },
+     {
+         id: 3,
+         name: "Johnson Doe",
+         job: "Engineer",
+         img: "1.jpeg",
+         text: "loream Ipsum jajdasjdhasjdhajdhajhd."
+     }
+ ]
---
+++ script.js
@@ -0,0 +1,28 @@
+const reviews = [{
+        id: 1,
+        name: "John Doe",
+        job: "Engineer",
+        img: "1.jpeg",
+        text: "loream Ipsum jajdasjdhasjdhajdhajhd."
+    },
+    {
+        id: 2,
+        name: "Jonn Doe",
+        job: "Engineer",
+        img: "1.jpeg",
+        text: "loream Ipsum jajdasjdhasjdhajdhajhd."
+    },
+    {
+        id: 3,
+        name: "Johnson Doe",
+        job: "Engineer",
+        img: "1.jpeg",
+        text: "loream Ipsum jajdasjdhasjdhajdhajhd."
+    }
+];
+
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
